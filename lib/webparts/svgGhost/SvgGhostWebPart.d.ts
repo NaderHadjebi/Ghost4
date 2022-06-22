d9bd54c0e4c22ca831a0ddcf027755c42022a76d
Nader Hadjebi <60085977+NaderHadjebi@users.noreply.github.com>
2022-06-22 16:58:26 -0400

    Update SvgGhostWebPart.d.ts

--- lib/webparts/svgGhost/SvgGhostWebPart.d.ts
+++ lib/webparts/svgGhost/SvgGhostWebPart.d.ts
@@ -1,15 +1,9 @@
 import { Version } from '@microsoft/sp-core-library';
 import { BaseClientSideWebPart, IPropertyPaneConfiguration } from '@microsoft/sp-webpart-base';
 export interface ISvgGhostWebPartProps {
-    colorPants: string;
     colorHair: string;
     colorBelt: string;
     colorBuckle: string;
-    colorSkin: string;
-    colorCape: string;
-    colorDiaper: string;
-    colorShoes: string;
-    colorShirt: string;
     colorLogo: string;
     height: number;
 }
